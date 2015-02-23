f602ba3bb4f635a1876ef7ef44f14917bb871989
Bartosz Tarnawski <bartek.tarnawski@gmail.com>
2015-02-23 20:54:23 +0100

    Wont use pdfgen

--- comm/views.py
+++ comm/views.py
@@ -1,4 +1,4 @@
-from reportlab.pdfgen import canvas
+#from reportlab.pdfgen import canvas
 
 from django.http import HttpResponse, HttpResponseForbidden
 from django.utils import timezone
@@ -23,14 +23,14 @@ def generate_report(request, communion_day_id):
             'attachment; filename="%s.pdf"' % filename
 
     # Create the PDF object, using the response object as its "file."
-    p = canvas.Canvas(response)
+    #p = canvas.Canvas(response)
 
     # Draw things on the PDF. Here's where the PDF generation happens.
     # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
+    #p.drawString(100, 100, "Hello world.")
 
     # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
+    #p.showPage()
+    #p.save()
     
     return response
